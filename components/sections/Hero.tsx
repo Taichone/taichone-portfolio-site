68cbbfe3c11e95a48e6a5fd6d783501a7b84121d
Taichone <taichone.dev@gmail.com>
2025-05-24 19:27:51 +0900

    update: hero のコメント

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -10,12 +10,8 @@ export default function Hero() {
 					三木 太智
 				</h1>
 				<h2 className="text-lg md:text-xl lg:text-2xl text-gray-600 dark:text-gray-400 mb-6 md:mb-8">
-					ソフトウェアエンジニア
+					名古屋大学 情報学研究科 修士2年
 				</h2>
-				<p className="max-w-3xl mx-auto text-base sm:text-lg md:text-xl leading-relaxed mb-8 md:mb-10">
-					名古屋大学 情報学研究科 修士2年 <br />
-					Swiftのアプリケーション開発に関心があります
-				</p>
 				<div className="flex justify-center gap-4">
 					<AppleButton
 						onClick={() => {
