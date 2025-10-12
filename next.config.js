353e946b790fce4ccc70a437fd5486380099166a
Taichone <taichone.dev@gmail.com>
2025-10-13 02:58:20 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,38 +4,6 @@ const nextConfig = {
 		remotePatterns: [
 			{
 				protocol: "https",
-				hostname: "encrypted-tbn0.gstatic.com",
-			},
-			{
-				protocol: "https",
-				hostname: "prtimes.jp",
-			},
-			{
-				protocol: "https",
-				hostname: "www.tokushima-u.ac.jp",
-			},
-			{
-				protocol: "https",
-				hostname: "illustimage.com",
-			},
-			{
-				protocol: "https",
-				hostname: "pbs.twimg.com",
-			},
-			{
-				protocol: "https",
-				hostname: "play-lh.googleusercontent.com",
-			},
-			{
-				protocol: "https",
-				hostname: "images.seeklogo.com",
-			},
-			{
-				protocol: "https",
-				hostname: "coupon.rakuten.co.jp",
-			},
-			{
-				protocol: "https",
 				hostname: "kunitori-jp.net",
 			},
 		],
